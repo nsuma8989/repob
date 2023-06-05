545c084ddae8bb00bbc07bb79184f2a5264e8d81
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-06-05 15:47:15 -0700

    Create json.sh

---
+++ src/json.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Function to generate JSON files
+generate_json_file() {
+    filename=$1
+    data=$2
+
+    echo "$data" > "$filename.json"
+    echo "Generated $filename.json"
+}
+
+# Generate JSON files
+generate_json_file "file1" '{"name": "John", "age": 25}'
+generate_json_file "file2" '{"name": "Jane", "age": 30}'
