5612c1206ba29508f8639cf772f79e19a64d4ed0
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-06-09 15:18:08 -0700

    Update joblog.sh

--- joblog.sh
+++ joblog.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl -H "Authorization: Bearer $TOKEN" "https://api.buildkite.com/v2/organizations/sumatestorg/pipelines/artifact_test/builds/$BUILDKITE_BUILD_NUMBER/jobs/$BUILDKITE_JOB_ID/log"
