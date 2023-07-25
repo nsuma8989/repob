0ff50fae3c46fec5a03b078670c6264889832afa
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-07-25 15:38:14 -0700

    Add files via upload

---
+++ src/generated_pipeline.sh
@@ -0,0 +1,38 @@
+cat <<EOF >> ${GENERATED_PIPELINE}
+env:
+ TERRITORY: $TERRITORY
+ CELL: $CELL
+ BACKEND_GIT_REF: $BACKEND_GIT_REF
+ SEED_SNAPSHOT: ${SEED_SNAPSHOT:-""}
+ ATLAS_HTTP_USERNAME_DIGEST: ${ATLAS_HTTP_USERNAME_DIGEST:-""}
+ ATLAS_HTTP_PASSWORD_DIGEST: ${ATLAS_HTTP_PASSWORD_DIGEST:-""}
+ MONGODB_TEST_SHELL_URL: ${MONGODB_TEST_SHELL_URL:-""}
+
+retry: &retry
+  automatic:
+  - exit_status: -1  # Agent was lost
+    limit: 2
+  - signal_reason: agent_stop
+    limit: 2
+  - exit_status: 255 # Forced agent shutdown
+    limit: 2
+  - exit_status: 20 # ArgoCD failures
+    limit: 2
+
+steps:
+  - label: "Disabling the deployment"
+    command: sleep 1
+    retry: *retry
+
+  - wait
+
+  - label: "reset showcase database"
+    timeout_in_minutes: 2
+    command: sleep 120
+    retry: *retry
+
+EOF
+
+cat "${GENERATED_PIPELINE}"
+
+buildkite-agent pipeline upload "${GENERATED_PIPELINE}"
