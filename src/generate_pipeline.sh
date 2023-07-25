15ad1ca95605548990d2e44e0c39a8d71fcf5d8a
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-07-25 15:11:14 -0700

    Update generate_pipeline.sh

--- src/generate_pipeline.sh
+++ src/generate_pipeline.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # exit immediately on failure, or if an undefined variable is used
-set -eu
+set -e
 
 # begin the pipeline.yml file
 echo "steps:"
-echo "  - command: sleep 120"
-echo "    label: timeout test"
-echo "    timeout_in_minutes: 2"
+cat <<YAML
+- command: sleep 120
+  label: timeout test
+  timeout_in_minutes: 2
+YAML
