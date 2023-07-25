52678d1ce7b7efa50091d62f0dc8702016645210
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-07-25 14:44:41 -0700

    Create generate_pipeline.sh

---
+++ .src/generate_pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+# begin the pipeline.yml file
+echo "steps:"
+echo "  - command: sleep 120"
+echo "    label: timeout test"
+echo "    timeout_in_minutes: 2"
+done
